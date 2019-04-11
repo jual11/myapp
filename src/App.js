c8048a49d1ea33b7c1c360fb8f564435ce960e02
Kasutaja-PC\Kasutaja <alarijurtom@gmail.com>
2019-04-11 15:01:52 +0300

    'adi'

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         e.preventDefault(); //Prevevents page refres
         const city = e.target.elements.city.value; //Takes input city value from form.js
         const country = e.target.elements.country.value; //Takes input country value from form.js
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`); //Makes api request
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`); //Makes api request
         const data = await api_call.json(); //Makes api request to json file
         if (city && country) { //If city and country has value
             this.setState({ //It changes states
