25c789ed26c617d5e38f6dbae0c768729e62570b
Kasutaja-PC\Kasutaja <alarijurtom@gmail.com>
2019-04-11 08:49:57 +0300

    Updates

---
+++ precache-manifest.3916224aa066586a4d6157e64625f809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/myapp/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "2c0a977fa998b587062d",
+    "url": "/myapp/static/js/main.8f4c8471.chunk.js"
+  },
+  {
+    "revision": "a65f59a93b26c2de44ac",
+    "url": "/myapp/static/js/2.bbac7bca.chunk.js"
+  },
+  {
+    "revision": "2c0a977fa998b587062d",
+    "url": "/myapp/static/css/main.835ee5dd.chunk.css"
+  },
+  {
+    "revision": "d2c7fed7c1fda9bcd32e8facb3d70c72",
+    "url": "/myapp/index.html"
+  }
+];
